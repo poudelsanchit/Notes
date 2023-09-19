a9fcede9d337ba9d24c663f16ddd7aeabbe377bf
Sanchit Poudel <snchtpdl12@gmail.com>
2023-09-19 20:51:03 +0545

    update EditNotes.jsx

--- src/Pages/EditNotes.jsx
+++ src/Pages/EditNotes.jsx
@@ -86,29 +86,31 @@ const EditNotes = ({handleEditNote,data}) => {
   },[])  
     return (
       <>
-      <div className='p-3 flex flex-col'>
+ <div className='flex flex-col w-full items-center dark:bg-primary bg-[#ffffff]'>       
+        <div className='w-3/5 '>
+        <div className='p-3 flex flex-col h-screen'>
           <div className='flex justify-between text-2xl'>
-              <IoChevronBack onClick={()=>navigate(-1)} className='cursor-pointer'/>
-            <IoCheckmarkDoneCircleOutline className='text-3xl cursor-pointer' onClick={saveData}/>
+              <IoChevronBack onClick={()=>navigate(-1)} className='cursor-pointer dark:text-[#f2f5fa] text-switch-bg'/>
+            <IoCheckmarkDoneCircleOutline className='text-3xl cursor-pointer dark:text-[#f2f5fa] text-switch-bg' onClick={saveData}/>
           </div>
           <div className='mt-5 font-Roboto font-bold text-2xl'>
             <div className='text-xs font-light text-[#FF0000]'>{errMsg}</div>
               <div>
-                  <input type="text" placeholder='title...' className='focus:outline-none bg-primary' value={title} onChange={(e)=>setTitle(e.target.value)} />
+                  <input type="text" placeholder='title...' className='focus:outline-none text-[#000000] dark:text-[#dce1e7]  placeholder:dark:text-[#dce1e7] placeholder:text-[#000000] dark:bg-primary bg-[#ffffff]' value={title} onChange={(e)=>setTitle(e.target.value)} />
               </div>
-              <div className='text-xs text-[#a2a7b5] font-light border-b-[1px] pb-2'>{   `${days[datetime.getDay()]},${datetime.toLocaleString("en-US", {hour: "numeric", minute: "numeric", hour12: true,})} | ${notelength} characters`}</div>
+              <div className='text-xs text-[#000000] dark:text-[#a2a7b5] font-light border-b-[1px] pb-2'>{   `${days[datetime.getDay()]},${datetime.toLocaleString("en-US", {hour: "numeric", minute: "numeric", hour12: true,})} | ${notelength} characters`}</div>
              
-              <div className='flex text-3xl bg-primary rounded-md items-center mt-4'>
-                    <textarea onChange={(event) => { setNote(event.target.value) }} value={note} type="text" name="" id="" className='bg-primary w-full focus:outline-none rounded-md text-sm font-medium l:text-lg h-96 ' placeholder='your note.....'  />
+              <div className='flex text-3xl dark:bg-primary bg-[#ffffff] rounded-md items-center mt-4'>
+                    <textarea onChange={(event) => { setNote(event.target.value) }} value={note} type="text" name="" id="" className='bg-[#ffffff] dark:bg-primary text-[#000000] dark:text-[#dce1e7]  placeholder:dark:text-[#dce1e7] placeholder:text-[#000000] w-full focus:outline-none rounded-md text-sm font-medium l:text-lg h-96 ' placeholder='your note.....'  />
               </div>
           </div>
           
       </div>
-      <div className='bg-[#171c26] bottom-0 absolute h-48 p-4 rounded-t-[2rem] w-full md:w-3/5'>
+      <div className='dark:bg-[#171c26] bg-[#dde1fa] bottom-0 absolute h-48 p-4 rounded-t-[2rem] w-full md:w-3/5'>
       <div className='flex justify-evenly '>
         { colors.map((notes,index)=>(
           <div className='flex flex-col items-center gap-1'>
-             <div className='w-10 h-10 rounded-full bg-[#212734] flex items-center justify-center'>     
+             <div className='w-10 h-10 rounded-full dark:bg-[#212734] bg-[#f2f5fa]  flex items-center justify-center'>     
                <div className='w-8 h-8 rounded-full p-2 cursor-pointer' style={{backgroundColor:notes.color}}  onClick={()=>setWhichColor(notes.color,index)}/>
           </div>
           <div className='w-full h-1 rounded-md ' style={notes.isActive?{backgroundColor:notes.color}:null}/>
@@ -117,19 +119,24 @@ const EditNotes = ({handleEditNote,data}) => {
                  ))
         }
       </div>
-      <div className='px-5 py-3  tracking-tighter	cursor-pointer flex items-center gap-2 text-lg font-Roboto'>
+      <div className='px-5 py-3  tracking-tighter	cursor-pointer flex items-center gap-2 text-lg font-Roboto dark:text-[#dce1e7] 
+      text-[#000000]'>
       <BsTrash/>
       <div>
         Delete Note
       </div>
         </div>
-        <div className='px-5 py-3 tracking-tighter	 cursor-pointer flex items-center gap-2 text-lg font-Roboto'>
+        <div className='px-5 py-3 tracking-tighter	 cursor-pointer flex items-center gap-2 text-lg font-Roboto dark:text-[#dce1e7] 
+      text-[#000000]'>
       <BsShare/>
       <div>
         Share
       </div>
         </div>
           </div>
+        </div>
+      </div>
+     
           </>
     )
   }
