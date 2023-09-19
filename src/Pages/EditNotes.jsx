b493ab890597cbb65f3b8035eb0b7ee98d9aec99
Sanchit Poudel <snchtpdl12@gmail.com>
2023-09-19 21:20:36 +0545

    update EditNotes.jsx

--- src/Pages/EditNotes.jsx
+++ src/Pages/EditNotes.jsx
@@ -87,7 +87,7 @@ const EditNotes = ({handleEditNote,data}) => {
     return (
       <>
  <div className='flex flex-col w-full items-center dark:bg-primary bg-[#ffffff]'>       
-        <div className='w-3/5 '>
+        <div className='md:w-3/5  w-full'>
         <div className='p-3 flex flex-col h-screen'>
           <div className='flex justify-between text-2xl'>
               <IoChevronBack onClick={()=>navigate(-1)} className='cursor-pointer dark:text-[#f2f5fa] text-switch-bg'/>
