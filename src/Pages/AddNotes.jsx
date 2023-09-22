740db57bdb82c7b7fde52309a6be0cb6c5dda97e
Sanchit Poudel <snchtpdl12@gmail.com>
2023-09-22 19:49:32 +0545

    update AddNotes.jsx and EditNotes.jsx

--- src/Pages/AddNotes.jsx
+++ src/Pages/AddNotes.jsx
@@ -109,7 +109,7 @@ const AddNotes = ({setNotes,data}) => {
           
       </div>
       
-        <div className='dark:bg-[#171c26] bg-[#171c26] bottom-0 fixed h-min p-4 rounded-t-[2rem] w-full md:w-3/5'>
+        <div className='dark:bg-[#171c26] bg-[#171c26] bottom-0 absolute h-min p-4 rounded-t-[2rem] w-full md:w-3/5'>
       <div className='flex justify-between '>
       { colors.map((notes,index)=>(
           <div className='flex flex-col items-center gap-1'>
